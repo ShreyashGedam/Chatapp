c5ade9ec3f6aa443f62cdaf054b902e3086fecef
shreyash-actyv <124993708+shreyash-actyv@users.noreply.github.com>
2023-03-06 10:36:54 +0530

    formatted

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState } from 'react';
-import { Login } from './components/Login';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import { Login } from "./components/Login";
 
 function App() {
-
-  const [id, setId] = useState('')
+  const [id, setId] = useState("");
 
   return (
-    <div style={{
-      width: '70%', margin: 'auto',
-      border: '1px solid', height: '100vh'
-    }} >
+    <div
+      style={{
+        width: "70%",
+        margin: "auto",
+        border: "1px solid",
+        height: "100vh",
+      }}
+    >
       {id}
       <Login setUserId={setId}></Login>
     </div>
