619e9adf208e7f81108e7d4e6bb6f552576e2a39
Shreyash Gedam <shreyashakgedam@gmail.com>
2023-03-06 09:34:59 +0530

    first chat commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import { Login } from './components/Login';
+
 
 function App() {
+
+  const [id, setId] = useState('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      width: '70%', margin: 'auto',
+      border: '1px solid', height: '100vh'
+    }} >
+      {id}
+      <Login setUserId={setId}></Login>
     </div>
   );
 }
